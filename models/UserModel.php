099ed72cd706866560b3fbee2971add78ec7fc6f
stradimarius <mariusclement68@gmail.com>
2024-11-26 19:24:08 +0100

    userModel

--- models/UserModel.php
+++ models/UserModel.php
@@ -1,5 +1,22 @@
 <?php
 
+use PDO;
+use PDOException;
+use DateTime;
 
+require_once __DIR__ . '/../config/database.php';
 
-?>
+class UserModel
+{
+
+    private $pdo;
+
+    public function __construct()
+    {
+        try {
+            $this->pdo = getDatabaseConnection();
+        } catch (PDOException $e) {
+            die("Error database connection : " . htmlspecialchars($e->getMessage()));
+        }
+    }
+}
