12e321f0709ce618f519ffd3e0eabc67d338c322
stradimarius <mariusclement68@gmail.com>
2024-11-26 19:32:25 +0100

    userModel

--- models/UserModel.php
+++ models/UserModel.php
@@ -19,4 +19,26 @@ class UserModel
             die("Error database connection : " . htmlspecialchars($e->getMessage()));
         }
     }
+
+    public function verifUser($email, $password)
+    {
+        try{
+            $stmt = $this->pdo->prepare("SELECT id, password FROM users WHERE email = :email");
+            $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+            $stmt->execute();
+
+            if($stmt->rowCount()===1) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if(password_verify($password, $user['password'])) {
+                    return $user;
+                } else {
+                    return "The mail or the password is incorrect.";
+                } catch (PDOException $e) {
+                    error_log("Error with the user's authentication : " . $e->getMessage());
+                    return "Internal mistake, plese try again later.";
+                }
+            }
+        }
+    }
 }
