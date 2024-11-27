18a648fbd91af984bdf041e9a9e70ef8d07bebe0
stradimarius <mariusclement68@gmail.com>
2024-11-27 18:15:57 +0100

    registerPage

--- views/register.php
+++ views/register.php
@@ -39,6 +39,25 @@ unset($_SESSION['register_error'], $_SESSION['success_message']);
         <div class="error"><?php echo htmlspecialchars($success); ?></div>
     <?php endif; ?>
 
+<form action="" method="post" enctype="multipart/form-data">
+
+        <label for="name">First Name&nbsp;:</label>
+        <input type="text" id="name" name="first_name" placeholder="First Name" value="<?php echo htmlspecialchars($_POST['first_name'] ?? ''); ?>" required>
+
+        <label for="lastName">Last Name&nbsp;:</label>
+        <input type="text" id="lastName" name="last_name" placeholder="Last Name" value="<?php echo htmlspecialchars($_POST['last_name'] ?? ''); ?>" required>
+
+        <label for="start">Birthday&nbsp;:</label>
+        <input type="date" id="start" name="trip-start" value="<?php echo htmlspecialchars($_POST['trip-start'] ?? ''); ?>" required>
+
+        <label for="name">First Name&nbsp;:</label>
+        <input type="text" id="name" name="first_name" placeholder="First Name" value="<?php echo htmlspecialchars($_POST['first_name'] ?? ''); ?>" required>
+
+        <label for="name">First Name&nbsp;:</label>
+        <input type="text" id="name" name="first_name" placeholder="First Name" value="<?php echo htmlspecialchars($_POST['first_name'] ?? ''); ?>" required>
+
+</form>
+
 </body>
 
 </html>
