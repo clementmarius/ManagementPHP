6d57f46eeb715b293ba5644352e76a9c76b9294b
stradimarius <mariusclement68@gmail.com>
2024-11-28 11:12:34 +0100

    Login

--- views/login.php
+++ views/login.php
@@ -1,12 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../public/assets/css/login.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 </head>
+
 <body>
     <h2>Login Page</h2>
+
+    <form action="" method="">
+        <label for="email">Email&nbsp;</label>
+        <input type="email" id="email" name="email" placeholder="Email" value="" required>
+
+    </form>
 </body>
+
 </html>
