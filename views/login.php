eac6c964319f1aeb36899a87a9082870d1f2f60d
stradimarius <mariusclement68@gmail.com>
2024-11-28 11:09:33 +0100

    Login

---
+++ views/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../public/assets/css/login.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <h2>Login Page</h2>
+</body>
+</html>
