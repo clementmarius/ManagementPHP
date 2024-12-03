f41803463b8ff2304428c0e78255651bddff5fe7
stradimarius <mariusclement68@gmail.com>
2024-12-03 11:46:58 +0100

    router

--- controllers/Router.php
+++ controllers/Router.php
@@ -12,5 +12,22 @@ class Router
         $this->routes['GET'][$path] = $controllerMethod;
     }
 
-    
+    public function post($path, $controllerMethod)
+    {
+        $this->routes['POST'][$path] = $controllerMethod;
+    }
+
+    public function dispatch($uri, $requestMethod)
+    {
+        foreach ($this->routes[$requestMethod] as $path => $controllerMethod) {
+            if (preg_match("#^path$#", $uri, $matches)) {
+                $controller = new UserController();
+                $method = $controllerMethod;
+                array_shift($matches);
+                call_user_func_array([$controller, $method], $matches);
+                return;
+            }
+        }
+        echo 'Error 404 : La page n\'existe pas';
+    }
 }
