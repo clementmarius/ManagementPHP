b8ad2b26a85a512c051b15056639d9d5b66bf02d
stradimarius <mariusclement68@gmail.com>
2024-12-05 16:17:54 +0100

    css + login

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -2,17 +2,15 @@
 
 namespace App\Management\Controllers;
 
-use App\Management\Router\Router;
-
 class HomeController
 {
 
-    public function home()
+     public function home()
     {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
 
-        require_once __DIR__ . '/../views/home_page.php';
+        require_once __DIR__ . '/../views/home.php';
     }
 }
--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once __DIR__ . '/controllers/UserController.php';
 require_once __DIR__ . '/controllers/AuthController.php';
 require_once __DIR__ . '/Router/Router.php';
+require_once __DIR__ . '/Controllers/HomeController.php';
+
 
 use App\Management\Controllers\HomeController;
 use App\Management\Router\Router\Router;
