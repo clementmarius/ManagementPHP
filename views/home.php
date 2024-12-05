f250b9ad1fe6ffa4947d7170bb4e8329c2d23968
stradimarius <mariusclement68@gmail.com>
2024-12-05 16:34:07 +0100

    css + login

--- views/home.php
+++ views/home.php
@@ -2,14 +2,17 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../public/assets/css/home.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
 </head>
 <body>
-    <p>This is the home page while you are log out : </p>
-    <div>
-        <button onclick="location.href = 'register' ;"><li>Register</li></button>
-        <button onclick="location.href = 'login' ;"><li>Login</li></button>
+    <div id="parent">
+        <h2>This is the home page while you are log out:</h2>
+        <div id="btn">
+            <button class="btn" onclick="location.href = 'register';">Register</button>
+            <button class="btn" onclick="location.href = 'login';">Login</button>
+        </div>
     </div>
 </body>
 </html>
