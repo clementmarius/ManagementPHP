e0befde06eea030d83fb1b7a6cc27ea728c49dd8
stradimarius <mariusclement68@gmail.com>
2024-12-05 11:44:51 +0100

    router

--- index.php
+++ index.php
@@ -12,28 +12,19 @@ use App\Management\Router\Router\Router;
 $router = new Router();
 
 //Definir les routes
-/* $router->get('/register', 'App\Management\Controllers\UserController@register');
-*/
 
 $router->get(path: '/register', controllerMethod: 'App\Management\Controllers\UserController@register');
 $router->post(path: '/register', controllerMethod: 'App\Management\Controllers\UserController@register');
-
-/* $router->post('/register', 'App\Management\Controllers\UserController@register');
- */$router->get('/user/profile', 'App\Management\Controllers\UserController@showUserProfile');
+$router->get('/user/profile', 'App\Management\Controllers\UserController@showUserProfile');
 $router->get('/user/(\d+)', 'App\Management\Controllers\UserController@findUserId');
 $router->get('/test', function () {
     echo 'Test route works!';
 });
 
 // Dispatcher la requete 
-/* $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
- */
-/* $uri = str_replace('/PhpPoo/ManagementPHP', '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
- */
 
 $uri = trim(str_replace(['/PhpPoo/ManagementPHP', '.php'], '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)), '/');
 
 var_dump($uri);
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-
-$router->dispatch($uri, $requestMethod);
+$router->dispatch(uri: '/' . $uri, requestMethod: $requestMethod);
