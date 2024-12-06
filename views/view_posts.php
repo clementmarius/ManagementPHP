b1c4cfdeb3207aa55050919e8553daf03d07d2b5
stradimarius <mariusclement68@gmail.com>
2024-12-06 10:52:37 +0100

    view post

--- views/view_posts.php
+++ views/view_posts.php
@@ -1,14 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../public/assets/css/user.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HomePage</title>
 </head>
-<body>
-    <h2>This is home page, you will see the last publications</h2>
 
-    <button onclick="location.href = 'localhost/PhpPoo/ManagementPHP/views/create_post.php';" id="myButton" class="float-left submit-button" >Create Post</button>
+<body>
+    <div id="parent">
+        <h2>This is home page, you will see the last publications</h2>
+        <div id="btn">
+            <button class="btn" onclick="location.href = 'localhost/PhpPoo/ManagementPHP/views/create_post.php';" id="myButton" class="float-left submit-button">Create Post</button>
+            <button class="btn" onclick="location.href = 'localhost/PhpPoo/ManagementPHP/views/user_profile.php';" id="myButton" class="float-left submit-button">View Profile</button>
+        </div>
+
+    </div>
 
 </body>
+
 </html>
