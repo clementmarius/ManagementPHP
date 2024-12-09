69757d76f33fa81802100533228eda852371a098
stradimarius <mariusclement68@gmail.com>
2024-12-09 10:31:21 +0100

    view post

--- views/user_profile.php
+++ views/user_profile.php
@@ -31,6 +31,7 @@ if (session_status() === PHP_SESSION_NONE) {
     <p>User last name : <?php echo htmlspecialchars($user_data['last_name'] ?? ''); ?></p>
     <p>Date of Birth : <?php echo htmlspecialchars($user_data['date_of_birth'] ?? ''); ?></p>
     <p>Email : <?php echo htmlspecialchars($user_data['email'] ?? ''); ?></p>
+    <p>Email : <?php echo htmlspecialchars($user_data['email'] ?? ''); ?></p>
 </body>
 
 </html>
