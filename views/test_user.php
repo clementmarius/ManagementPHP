a551640e82974b6e4157100ad803415003d783ae
stradimarius <mariusclement68@gmail.com>
2024-12-12 16:14:12 +0100

    display user

--- views/test_user.php
+++ views/test_user.php
@@ -13,19 +13,30 @@ if (session_status() === PHP_SESSION_NONE) {
     error_log("Session démarrée.");
 }
 
+$user_data = $_SESSION['user_data'] ?? null;
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Test</title>
 </head>
-<body>
-<p>OK</p>
-<p>Test work</p>
-<p><strong>User Id : </strong><?php echo htmlspecialchars($user_data['id'] ?? '') ?></p>
 
+<body>
+    <p>OK</p>
+    <p>Test work</p>
+    <?php if ($user_data): ?>
+        <p>User first name : <?php echo htmlspecialchars($user_data['first_name'] ?? ''); ?></p>
+        <p>User last name : <?php echo htmlspecialchars($user_data['last_name'] ?? ''); ?></p>
+        <p>Date of Birth : <?php echo htmlspecialchars($user_data['date_of_birth'] ?? ''); ?></p>
+        <p>Email : <?php echo htmlspecialchars($user_data['email'] ?? ''); ?></p>
+    <?php else: ?>
+        <p>Aucune donnée utilisateur disponible.</p>
+    <?php endif; ?>
 </body>
+
 </html>
