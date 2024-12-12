2da1b73a50941d2b3a04bc597f4176ff1be121fe
stradimarius <mariusclement68@gmail.com>
2024-12-12 14:19:41 +0100

    css pico

--- views/home.php
+++ views/home.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="../public/assets/css/home.css">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
     <title>Home Page</title>
 </head>
+
 <body>
+
+<header class="class container">
+    <h1>Home Page</h1>
+    <p></p>
+</header>
+
+<!-- 
     <div id="parent">
         <h2>This is the home page while you are log out:</h2>
         <div id="btn">
@@ -14,5 +24,6 @@
             <button class="btn" onclick="location.href = 'login';">Login</button>
         </div>
     </div>
-</body>
+ --></body>
+
 </html>
